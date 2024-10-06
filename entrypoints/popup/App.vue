9a0945ecfe4347d2ba020bc40f792cc849b39835
Shahar Yair <shahar@firstofferz.com>
2024-10-06 11:26:10 +0300

    add loading state to popup refresh button

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -2,13 +2,28 @@
 import Button from 'primevue/button';
 import { sendSaveAvatarsAction } from '../../utils';
 
-const onRefreshClick = () => {
+const loading = ref<boolean>(false);
+const status = ref({
+  saved: false,
+});
+
+const onRefetchClick = () => {
+  loading.value = true;
   sendSaveAvatarsAction()
+  setTimeout(() => {
+    status.value.saved = true;
+    loading.value = false;
+  }, 1000);
+
+  setTimeout(() => {
+    status.value.saved = false;
+  }, 5000);
 }
 </script>
 
 <template>
-  <div class="w-48 p-4 flex flex-col justify-center items-center bg-zinc-800">
-    <Button @click="onRefreshClick" severity="primary" label="Refresh" rounded />
+  <div class="w-48 p-4 flex flex-col gap-2 justify-center items-center bg-zinc-800">
+    <Button @click="onRefetchClick" :loading="loading" severity="primary" label="Refetch" rounded />
+    <div v-if="status.saved">Saved!</div>
   </div>
 </template>
