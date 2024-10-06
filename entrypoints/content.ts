cdf8931d5aadf1aa369247b9758fe5cddb4ad3ef
Shahar Yair <shahar@firstofferz.com>
2024-10-06 11:26:41 +0300

    save avatar sources in localstorage

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,5 +1,5 @@
 import { ContentScriptContext } from "wxt/client";
-import { saveJiraAvatars, createVNodeAndRenderComponent } from "../utils";
+import { createVNodeAndRenderComponent } from "../utils";
 import RandomAvatar from "../components/randomAvatar.vue";
 import "./popup/style.css";
 import "primeicons/primeicons.css";
@@ -9,10 +9,11 @@ export default defineContentScript({
   main(ctx: ContentScriptContext) {
     if (ctx.isValid) {
       setTimeout(() => {
-        const avatarSources = saveJiraAvatars();
-        const componentParent = document.querySelector('[data-testid="software-filters.ui.list-filter-container"]') as HTMLElement;
-        createVNodeAndRenderComponent(RandomAvatar, componentParent, { avatarSources });
-      }, 250);
+        const componentParent = document.querySelector(
+          '[data-testid="software-filters.ui.list-filter-container"]'
+        ) as HTMLElement;
+        createVNodeAndRenderComponent(RandomAvatar, componentParent);
+      }, 1000);
     }
   },
 });
