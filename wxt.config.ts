a0efff404a589782a940367dd31c1fd21ab5672a
Shahar Yair <shaharyair12@gmail.com>
2024-12-11 10:41:07 +0200

    decrease wxt config tag

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   modules: ["@wxt-dev/module-vue"],
   manifest: {
     name: "Jira Avatars Randomizer",
-    version: "1.0",
+    version: "0.1.1",
     permissions: ["storage", "activeTab", "scripting"],
   },
 });
