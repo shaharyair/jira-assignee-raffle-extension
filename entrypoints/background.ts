f8c54d890f32f9e0322350e3438a575f504d284a
Shahar Yair <shaharyair12@gmail.com>
2026-09-11 23:50:36 +0300

    build: modernize toolchain and drop unused UI deps
    Update wxt 0.19 -> 0.21, vue 3.4 -> 3.5 and reinstall from scratch.
    Drop primevue, @primevue/themes, primeicons and @vueuse/core: they served
    a popup that is being removed, and tailwind is no longer needed for a
    single 24px button.

--- entrypoints/background.ts
+++
@@ -1,6 +0,0 @@
-import { ChromeUtils } from "@/utils/chrome/utils";
-import { EventActions } from "@/utils/chrome/types";
-
-export default defineBackground(() => {
-  ChromeUtils.addEventListener(EventActions.SAVE_AVATARS, ChromeUtils.executeScriptOnActiveTab, ["saveJiraAvatars.js",]);
-});
